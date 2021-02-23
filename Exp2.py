05bcf25667cec29df317a5cbebfa8ea23f1b84f0
Christianne Orrico Dalforno <profcod@gmail.com>
2021-02-23 15:07:13 -0300

    terminei Exp2

--- Exp2.py
+++ Exp2.py
@@ -3,6 +3,12 @@ import pandas as pd
 from sklearn import datasets
 from sklearn import svm
 
+def coding(col, codeDict):
+  colCoded = pd.Series(col, copy=True)
+  for key, value in codeDict.items():
+    colCoded.replace(key, value, inplace=True)
+  return colCoded
+
 df = pd.read_csv('zombie-health-symptoms-diagnostic.csv')
 
 print(df.head())
